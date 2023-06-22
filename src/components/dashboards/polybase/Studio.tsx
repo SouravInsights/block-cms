cc5f305083c998871dc2fba058b954cc2b73740b
Sahil Nanda <kumarsahilnanda@gmail.com>
2023-06-22 17:56:48 +0530

    Merge conflicts Resolved

--- src/components/dashboards/polybase/Studio.tsx
+++ src/components/dashboards/polybase/Studio.tsx
@@ -1,10 +1,7 @@
 import { Box, Flex } from "@chakra-ui/react";
 import Sidebar from "./sidebar/SideBar";
-<<<<<<< Updated upstream
-=======
 import ModelDrawer from "./sidebar/ModelDrawer";
 import { Welcome } from "./Welcome";
->>>>>>> Stashed changes
 
 const Studio = () => {
   return (
--- src/components/dashboards/polybase/sidebar/SideBar.tsx
+++ src/components/dashboards/polybase/sidebar/SideBar.tsx
@@ -14,99 +14,37 @@ import {
   FiSettings,
   FiAlertCircle,
 } from "react-icons/fi";
-<<<<<<< Updated upstream
-import {
-  BiLayer,
-  BiRocket,
-  BiChevronLeftSquare,
-  BiMenu,
-} from "react-icons/bi";
-import SideBarItem from "./SideBarItem";
-=======
 import { BiLayer, BiRocket, BiChevronLeftSquare, BiMenu } from "react-icons/bi";
-import SideBarItem from "../SideBarItem";
->>>>>>> Stashed changes
+import SideBarItem from "./SideBarItem";
 import ModelDrawer from "./ModelDrawer";
 const SideBarItems = [
   {
-    id : 1,
-    name : 'Schema',
-    itemIcon : BiLayer
+    id: 1,
+    name: "Schema",
+    itemIcon: BiLayer,
   },
   {
-    id : 2,
-    name : 'Content',
-    itemIcon : FiFileText
+    id: 2,
+    name: "Content",
+    itemIcon: FiFileText,
   },
   {
-    id : 3,
-    name : 'Files',
-    itemIcon : FiPaperclip
+    id: 3,
+    name: "Files",
+    itemIcon: FiPaperclip,
   },
   {
-    id : 4,
-    name : 'API Plyaground',
-    itemIcon : BiRocket
+    id: 4,
+    name: "API Plyaground",
+    itemIcon: BiRocket,
   },
-  
-]
+];
 
 const Sidebar = ({ formData }: any) => {
   const [navSize, changeNavSize] = useState("large");
   const [isSelected, setSelect] = useState(false);
   const [heading, setHeading] = useState("jasbdfaiu");
   return (
-<<<<<<< Updated upstream
-    <Flex flexDir= 'row' >
-    <Flex
-      pos="sticky"
-      h="88vh"
-      marginTop="2.5vh"
-      boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
-      w={navSize == "small" ? "75px" : "262px"}
-      flexDir="column"
-      justifyContent="space-between"
-      bg={"gray.100"}
-    >
-    <VStack mt={5} >
-        <IconButton
-          aria-label={""}
-          mr={5}
-          fontSize={"2rem"}
-          onClick={() => {
-            if (navSize == "small") changeNavSize("large");
-            else changeNavSize("small");
-           }
-            }
-          as={navSize == "small"? (
-           BiMenu
-              ) : (
-           BiChevronLeftSquare
-             )}
-       />
-       <HStack mt={30}>
-        <IconButton
-          aria-label={""}
-          alignItems={navSize == "small" ? "center" : "flex-start"}
-          >
-          <Square bg="Black" size="60px">
-            <Text color="white"> demo</Text>
-          </Square>
-        </IconButton>
-        <Flex
-          flexDir="column"
-          display={navSize == "small" ? "none" : "flex"}
-        >
-          <Text fontSize="md" as="b">
-            Demo Project
-          </Text>
-          <Text fontSize="xs">Demo Project</Text>
-        </Flex>
-      </HStack>
-    </VStack>
-        <Flex
-        p="5%"
-=======
     <Flex flexDir="row">
       <Flex
         pos="sticky"
@@ -114,97 +52,42 @@ const Sidebar = ({ formData }: any) => {
         marginTop="2.5vh"
         boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
         w={navSize == "small" ? "75px" : "262px"}
->>>>>>> Stashed changes
         flexDir="column"
         justifyContent="space-between"
         bg={"gray.100"}
       >
-<<<<<<< Updated upstream
-          {SideBarItems.map((item) =>{
-            return(
-              <SideBarItem
-                key={item.id}
-                icon={item.itemIcon}
-                title={item.name} 
-                active={false} 
-                navSize={navSize} 
-                onClick={()=>{
-                  setSelect(true)
-                  setHeading(item.name)
-                }}                
-              />
-            );
-
-          }
-          )}
-        </Flex>
-      <Flex
-        p="5%"
-        flexDir="column"
-        w="100%"
-        alignItems={navSize == "small" ? "center" : "flex-start"}
-        mb={4}
-      >
-        <Divider
-          color={"black"}
-          display={navSize == "small" ? "none" : "flex"}
-        />
-        <Flex flexDir="column">
-          <SideBarItem
-            navSize={navSize}
-            icon={FiSettings}
-            title="Project Setting"
-            active={false}
-            onClick ={
-              ()=>{
-                setHeading('Project Setting')
-             }
-            }
-          />
-          <SideBarItem
-            navSize={navSize}
-            icon={FiAlertCircle}
-            title="Help"
-            active={false}
-            onClick ={
-              ()=>{
-                setHeading('Help')
-             }
-            }
-=======
-        <Flex flexDir="row" p="5%">
+        <VStack mt={5}>
           <IconButton
             aria-label={""}
-            alignItems={navSize == "small" ? "center" : "flex-start"}
-            pt={navSize == "small" ? "5" : ""}
-          >
-            <Square bg="Black" size="60px">
-              <Text color="white"> demo</Text>
-            </Square>
-          </IconButton>
-          <Flex
-            flexDir="column"
-            pl="5"
-            display={navSize == "small" ? "none" : "flex"}
-          >
-            <p>{formData.projectName}</p>
-
-            {/*<Text fontSize="md" as="b">
-              Demo Project
-            </Text>
-            <Text fontSize="xs">Demo Project</Text>*/}
-          </Flex>
-          <IconButton
-            aria-label={""}
+            mr={5}
             fontSize={"2rem"}
             onClick={() => {
               if (navSize == "small") changeNavSize("large");
               else changeNavSize("small");
             }}
-          >
-            {navSize == "small" ? <BiMenu /> : <BiChevronLeftSquare />}
-          </IconButton>
-        </Flex>
+            as={navSize == "small" ? BiMenu : BiChevronLeftSquare}
+          />
+          <HStack mt={30}>
+            <IconButton
+              aria-label={""}
+              alignItems={navSize == "small" ? "center" : "flex-start"}
+            >
+              <Square bg="Black" size="60px">
+                <Text color="white"> demo</Text>
+              </Square>
+            </IconButton>
+            <Flex
+              flexDir="column"
+              display={navSize == "small" ? "none" : "flex"}
+            >
+              <p>{formData.projectName}</p>
+              {/*<Text fontSize="md" as="b">
+                Demo Project
+              </Text>
+              <Text fontSize="xs">Demo Project</Text>*/}
+            </Flex>
+          </HStack>
+        </VStack>
         <Flex
           p="5%"
           flexDir="column"
@@ -212,45 +95,22 @@ const Sidebar = ({ formData }: any) => {
           alignItems={navSize == "small" ? "center" : "flex-start"}
           mb={4}
         >
-          <SideBarItem
-            navSize={navSize}
-            icon={BiLayer}
-            title="Schema"
-            active={false}
-            onClick={() => {
-              setHeading("Schema");
-              setSelect(true);
-            }}
-          />
-          <SideBarItem
-            navSize={navSize}
-            icon={FiFileText}
-            title="Content"
-            active={true}
-            onClick={() => {
-              setHeading("Content");
-            }}
-          />
-          <SideBarItem
-            navSize={navSize}
-            icon={FiPaperclip}
-            title="Files"
-            active={false}
-            onClick={() => {
-              setHeading("Files");
-            }}
-          />
-          <SideBarItem
-            navSize={navSize}
-            icon={BiRocket}
-            title="API Playground"
-            active={false}
-            onClick={() => {
-              setHeading("API Playground");
-            }}
-          />
+          {SideBarItems.map((item) => {
+            return (
+              <SideBarItem
+                key={item.id}
+                icon={item.itemIcon}
+                title={item.name}
+                active={false}
+                navSize={navSize}
+                onClick={() => {
+                  setSelect(true);
+                  setHeading(item.name);
+                }}
+              />
+            );
+          })}
         </Flex>
-
         <Flex
           p="5%"
           flexDir="column"
@@ -261,7 +121,6 @@ const Sidebar = ({ formData }: any) => {
           <Divider
             color={"black"}
             display={navSize == "small" ? "none" : "flex"}
->>>>>>> Stashed changes
           />
           <Flex flexDir="column">
             <SideBarItem
@@ -285,14 +144,12 @@ const Sidebar = ({ formData }: any) => {
           </Flex>
         </Flex>
       </Flex>
-      {isSelected ? <ModelDrawer /> : ""}
+      {isSelected && heading !== "API Plyaground" ? (
+        <ModelDrawer heading={heading} />
+      ) : (
+        ""
+      )}
     </Flex>
-<<<<<<< Updated upstream
-    {isSelected && heading !== 'API Plyaground'?
-    (<ModelDrawer heading={heading} />):('')}
-  </Flex>
-=======
->>>>>>> Stashed changes
   );
 };
 
